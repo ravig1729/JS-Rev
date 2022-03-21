e081c68612adf26e76e14e15ba89f5440ac14da6
ravig1729 <ravigedela1729@gmail.com>
2022-03-21 15:02:02 +0530

    js3

---
+++ JS3/index.js
@@ -0,0 +1,42 @@
+// alert("Welcome to JS3");
+
+//Airthamtic 
+// var x=5;
+// var y =10;
+// var z= x+y;  //x-y ; x*y ; y/x ; y%x
+// console.log(z);
+
+//Increment and Decrement 
+// var x=5;
+// console.log(--x);  //x++ ; x-- ; ++x ; --x
+
+//Comparison :
+// var a=5, b=10, c= '5'
+// console.log(a <= b);  // a==c -true ; a===c - false ; a!=c -false ;a!==c -true ; a>b ;a<b -true; a>=b -false; a<=b -true
+
+//Logical Operators
+// var a=5, b=10;
+// console.log( a==b && a<b);  //false
+// console.log( a==b || a<b);  //true
+// console.log (!( a==b || a<b));  //false
+
+//Assignment 
+// var x=5 , y=10;
+// x = y // x value will be y
+// console.log(x);  //10
+// console.log( x+=1);  // x=x+1  --> 6
+// console.log( x-=1);  // x=x-1  --> 4
+// console.log( x*=1);
+// console.log( x/=2);
+// console.log( x%=2);
+
+//Ternary
+// var a=10, b=15;
+// var c= (a==b) ? 'true' : 'false';
+
+//Bitwise
+// var a=2 , b=5;  // it checks the truth table by taking 64-bit comparison.
+// // var c= ~a;  // a&b -0 , a|b -7 , a^b -7, ~b -(-6)
+// var c = a<<b ;  //a>>b -0, a<<b -64, a>>>b -0
+// console.log(c);
+// in bitwise operators we get values.
