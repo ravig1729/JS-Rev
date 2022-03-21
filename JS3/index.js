c3ed8296a1a2e4e9d90cb9f96d86b271f93bc546
ravig1729 <ravigedela1729@gmail.com>
2022-03-21 15:23:47 +0530

    JS3 changes

--- JS3/index.js
+++ JS3/index.js
@@ -40,3 +40,23 @@
 // var c = a<<b ;  //a>>b -0, a<<b -64, a>>>b -0
 // console.log(c);
 // in bitwise operators we get values.
+
+// var a=-3;  //0000 0000 0000   0011  
+// var b=2;//0000 0000 0000 0010 
+
+// console.log(~a)
+
+// In Not operatror -->If  the value is positive its incremented by 1 and the value of  is positive so it will be changed to negative 
+// In Not operatror -->If  the value is negative  its decremented  by 1 and the value of  is negative  so it will be changed to positive  
+
+// a=+3 =>3+1==>-4
+// a=-3==>3-1==>+2
+
+//<<left shift operator
+console.log(1<<4);   //16
+// 1* 2^4 =16
+//>>right shift
+console.log(1>>4);
+// 1/2 ^4= 0
+
+
