4913fc752d7c53600707f05900eb02c9e014eb9e
ravig1729 <ravigedela1729@gmail.com>
2022-03-23 15:56:20 +0530

    js4 n 5

---
+++ JS5/index.js
@@ -0,0 +1,57 @@
+// var name = 'ravi'
+// console.log(name);
+
+// for(initialization ; consition ; increment/decrement)
+// i++ = forward loop ; i --  = backward loop
+
+// for (let i = 1; i <=10 ; i++) {
+//     console.log(i);
+// }
+
+//print 13 table
+
+for (let i=1; i<=10 ; i++){
+    console.log(13+ 'X'+i +'=' + 13*i)
+}
+
+//while:
+
+// initialisation
+// while(condition){
+//     increment/decrement;
+// }
+
+// var i=1;
+
+// while(i<=10){
+    
+//     console.log(13+ 'X'+i +'=' + 13*i);
+//     i++;
+// }
+
+// var num =12;
+// var lastdigit=0;
+// var sumofalldigits=0;
+
+// while(num!=0){
+
+//     var lastdigit = num % 10;
+//     sumofalldigits = sumofalldigits + lastdigit;
+//     num = (parseInt(num/10));
+
+// }
+// console.log(sumofalldigits);
+
+//do-while:
+// initialization ;
+// do{
+//     increment/decrement;
+// }
+// while(condition) ;
+
+// var i=1;
+
+// do{
+//     console.log(i);
+//     i++;
+// }while(i<=10);
